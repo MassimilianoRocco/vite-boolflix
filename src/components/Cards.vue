adc1fb6b3656050f31b7f7754ac7f3ee89d23d56
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-05-30 19:35:20 +0200

    Added: Hover Effect

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -60,22 +60,26 @@
       <img v-if="store.isMovie==true" :src="getImage()"  alt="movie img">
       <img v-else :src="getImage()"  alt="movie img">
 
-        <p v-if="store.isMovie==true">Title: {{singleMovie.title ?  singleMovie.title : 'No Name'}}</p>
-        <p v-else>Title: {{singleMovie.name ?  singleMovie.name : 'No Name'}}</p>
+        <div class="text_box">
+            <p v-if="store.isMovie==true"><b>Title: </b> <span class="text_from_data">{{singleMovie.title ?  singleMovie.title : 'No Name'}}</span></p>
+            <p v-else> <b>Title:</b> <span class="text_from_data">{{singleMovie.name ?  singleMovie.name : 'No Name'}}</span></p>
 
-        <p v-if="store.isMovie==true">Original Title: {{singleMovie.original_title ?  singleMovie.original_title : 'No Name'}}</p>
-        <p v-else>Original Name: {{singleMovie.original_name ?  singleMovie.original_name : 'No Name'}}</p>
+            <p v-if="store.isMovie==true"><b>Original Title: </b><span class="text_from_data">{{singleMovie.original_title ?  singleMovie.original_title : 'No Name'}}</span></p>
+            <p v-else><b>Original Name: </b><span class="text_from_data">{{singleMovie.original_name ?  singleMovie.original_name : 'No Name'}}</span></p>
+
+            <p v-if="store.isMovie==true"><b>Description: </b><br><span class="text_from_data">{{singleMovie.overview ?  singleMovie.overview : 'No Description'}}</span></p>
+            <p v-else><b>Description: </b><br><span class="text_from_data">{{singleMovie.overview ?  singleMovie.overview : 'No Description'}}</span></p>
+
+            <p v-if="store.isMovie==true"><b>Language: </b><span class="text_from_data">{{singleMovie.original_language ?  singleMovie.original_language : 'No Lang'}}</span></p>
+            <p v-else><b>Language: </b><span class="text_from_data">{{singleMovie.original_language ?  singleMovie.original_language : 'No Lang'}}</span></p>
 
 
-        <p v-if="store.isMovie==true">Language: {{singleMovie.original_language ?  singleMovie.original_language : 'No Lang'}}</p>
-        <p v-else>Language: {{singleMovie.original_language ?  singleMovie.original_language : 'No Lang'}}</p>
-
-
-        <p v-if="store.isMovie==true"><i class="fa-solid fa-star" style="color: #FFD43B;"></i> {{getFixedVote()}}</p>
-        <p v-else>
-          <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
-           {{getFixedVote()}}
-        </p>
+            <p v-if="store.isMovie==true"><i class="fa-solid fa-star" style="color: #FFD43B;"></i> {{getFixedVote()}}</p>
+            <p v-else>
+              <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+              {{getFixedVote()}}
+            </p>
+        </div>
       </div>
     </div>
 </template>
@@ -84,22 +88,45 @@
 <style scoped>
      .card{
         width: calc((100% / 6));
-        text-align: center;
         padding: 1rem;
+        position: relative;
+        cursor: pointer;
     }
     .card img{
         width: 100%;
-        height: 80%;
         object-fit: cover;
     }
     .movie_info_box{
       background-color:  #2e2d2d;
       color: white;
-      height: 500px;
-      overflow: auto;
     }
     .movie_info_box p{
-      margin: 4px 0;
-      font-size: 0.8rem;
+      max-width: 90%;
+      margin: 4px auto;
     }
+    .text_box{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      padding: 1rem;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      opacity: 0;
+      overflow: auto;
+    }
+    .card:hover img{
+      filter: opacity(30%);
+      transition: 1s;   
+    }
+    .card:hover .text_box{
+      opacity: 1;
+      transition: 1s;
+    }
+
+    .text_from_data{
+      color: rgb(218, 216, 216);
+    }
+
+
 </style>
